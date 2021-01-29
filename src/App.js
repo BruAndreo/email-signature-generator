a3a175be2aab07fd7ba3d5832cddfa99d580d9e9
Bruno Andreo <brunolino2026@gmail.com>
2021-01-28 22:09:17 -0300

    Added info form component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import { Container, Typography } from '@material-ui/core';
+import InfoForm from './components/InfoForm';
 
 function App() {
   return (
     <Container maxWidth="md">
 
-      <Typography 
-        variant="h3" 
-        component="h1" 
+      <Typography
+        variant="h3"
+        component="h1"
         align="center"> E-mail Signature Generator </Typography>
+
+      <InfoForm />
 
     </Container>
   );
---
+++ src/components/InfoForm/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function InfoForm() {
+  return (
+    <p>Info Form Component</p>
+  );
+};
+
+export default InfoForm;
