6b5985651eea70927f1facd4084cc12cf0e44865
Bruno Andreo <brunolino2026@gmail.com>
2021-02-04 20:59:27 -0300

    Added button to upload logo

--- src/components/InfoForm/index.js
+++ src/components/InfoForm/index.js
@@ -1,46 +1,54 @@
 import React from 'react';
-import { TextField } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 
 function InfoForm() {
   return (
     <form>
+      <Button
+        variant="contained"
+        component="label"
+      >
+        Upload
+        <input type="file" hidden />
+      </Button>
+
+      <TextField
+        id="name"
+        name="name"
+        label="Your Name"
+        required
+        variant="outlined"
+        fullWidth
+        size="small"
+      />
+
+      <TextField
+        id="position"
+        name="position"
+        label="Your Position"
+        variant="outlined"
+        fullWidth
+        size="small"
+      />
+
     <TextField
-      id="name"
-      name="name"
-      label="Your Name"
-      required
+        id="phone"
+        name="phone"
+        label="Phone Number"
+        type="number"
+        variant="outlined"
+        fullWidth
+        size="small"
+      />
+
+    <TextField
+      id="phase"
+      name="phase"
+      label="pahse"
       variant="outlined"
       fullWidth
       size="small"
     />
-
-    <TextField
-    id="position"
-    name="position"
-    label="Your Position"
-    variant="outlined"
-    fullWidth
-    size="small"
-  />
-
-  <TextField
-    id="phone"
-    name="phone"
-    label="Phone Number"
-    type="number"
-    variant="outlined"
-    fullWidth
-    size="small"
-  />
-
-  <TextField
-    id="phase"
-    name="phase"
-    label="pahse"
-    variant="outlined"
-    fullWidth
-    size="small"
-  />
   </form>
   );
 };
