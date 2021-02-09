ffdf29ac017e7ffcc0400b708538555499140166
Bruno Andreo <brunolino2026@gmail.com>
2021-02-08 21:41:00 -0300

    Added itens for email signature layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Container, Typography } from '@material-ui/core';
 import InfoForm from './components/InfoForm';
+import Logo from './assets/img/google.png';
 
 function App() {
   return (
@@ -12,6 +13,23 @@ function App() {
         align="center"> E-mail Signature Generator </Typography>
 
       <InfoForm />
+
+      <br />
+
+      <div>
+        <img
+          src={Logo}
+          width="140px"
+          height="100px"
+          alt="Logo" />
+
+        <div>
+          <h3>Bruno Lino Andreo</h3>
+          <p>Desenvolvedor de Sistemas</p>
+          <p>XX XXXX-XXXX</p>
+          <p>Mares calmos não formam bons marinheiros</p>
+        </div>
+      </div>
 
     </Container>
   );
